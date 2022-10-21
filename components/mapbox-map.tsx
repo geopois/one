296bb2234cf191e68d95f7e3d0ab219321288d18
Javi Gabás <jgabas@geopois.com>
2022-10-21 11:20:40 +0200

    add bbox

--- components/mapbox-map.tsx
+++ components/mapbox-map.tsx
@@ -9,6 +9,7 @@ import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 function MapboxMap() {
   const [map, setMap] = React.useState<mapboxgl.Map>();
   const mapNode = React.useRef(null);
+  
 
   React.useEffect(() => {
     const node = mapNode.current;
@@ -16,16 +17,19 @@ function MapboxMap() {
 
     const mapboxMap = new mapboxgl.Map({
       container: node,
-            accessToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN,
-            style: process.env.NEXT_PUBLIC_MAPBOX_STYLE,
+      accessToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN,
+      style: process.env.NEXT_PUBLIC_MAPBOX_STYLE,
       center: [-3.697457215656853, 40.348295056838715],
       zoom: 17,
       minZoom: 16,
+      maxBounds: [[-3.8382521729390406, 40.31206732887142],[-3.5181700059205485, 40.58359405482841]]
     });
 
     mapboxMap.addControl(
       new MapboxGeocoder({
         accessToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN as string,
+        countries: 'es',
+        bbox: [-3.8382521729390406, 40.31206732887142, -3.5181700059205485, 40.58359405482841],
         flyTo: {
           speed: 100 // Make the flying slow.
         },
